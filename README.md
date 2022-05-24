24f43e4b2be9303a3ffc1bc6e000462b722830e9
JessSkyWatcher21 <99293043+JessSkyWatcher21@users.noreply.github.com>
2022-05-24 12:20:16 -0700

    placed codepen code into VS

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # my-boilerPlate
 my boiler plate
+:Code starting point
+//Code placed from codepen
+
+:Reference
+// GreenSock spin effect found by: https://codepen.io/MAW/pen/ZbVWwN
+
+:Links used
+//https://cdn.jsdelivr.net/npm/circletype@2.3.0/dist/circletype.min.js
+//https://cdn.rawgit.com/orling/grapheme-splitter/b4500feb/index.js
+//https://unpkg.co/gsap@3/dist/gsap.min.js
--- my-boilerPlate/scripts.js
+++ my-boilerPlate/scripts.js
@@ -1,2 +1,16 @@
 // my scripts
 console.log('Loaded')
+const splitter = new GraphemeSplitter();
+const autum = new CircleType(
+  document.getElementById("autum"),
+  splitter.splitGraphemes.bind(splitter)
+);
+
+new CircleType(document.getElementById("text")).radius(384);
+
+TweenMax.to("#autum", 20, {
+  rotation: "+=360",
+  repeat: -1,
+  ease: Linear.easeNone,
+  transformOrigin: "50% 50%"
+});
